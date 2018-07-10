b5184fd89b7bf7c456c51080d98df0fcfd769b31
zeGolem <zegolem1383@outlook.fr>
2018-07-10 20:14:16 +0200

    Added Gui class for future GUIs

---
+++ idk/Gui.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace idk
+{
+    class Gui
+    {
+        public int X, Y, sX, sY;
+
+        public Gui(int posX, int posY, int sizeX, int sizeY)
+        {
+
+            X = posX;
+            Y = posY;
+            sX = sizeX;
+            sY = sizeY;
+
+        }
+
+        public bool isMouseOvering(MouseState mState)
+        {
+            if (mState.X > this.X && mState.X <= this.X + this.sX && mState.Y > this.Y && mState.Y < this.Y + this.sY)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
